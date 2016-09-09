6e70ee9229b6b05e92646adfffec0ec2dd20badc
Choi-PC\Choi <csj21300@naver.com>
2016-09-10 03:43:07 +0900

    v1.1 코딩한거 다 지워짐^^

--- src/main/java/com/hanium/myapp/GPS/Terminal_Location/Terminal_Location.java
+++ src/main/java/com/hanium/myapp/GPS/Terminal_Location/Terminal_Location.java
@@ -21,7 +21,8 @@ public class Terminal_Location {
 	public String initmessage()
 	{
 		String text = "환영합니다. 당신은 위치 확인을 선택하였습니다~\n" +
-					  "지역 선택을 해주세요.";
+					  "지역 선택을 해주세요.\n";
+				//	  "1. "
 			
 		this.text = text;
 		this.no = 3100; 
@@ -96,7 +97,6 @@ public class Terminal_Location {
 	        case "7" :
 	        	
 	        	break;
-	        
 	        
 	        }
 	        
