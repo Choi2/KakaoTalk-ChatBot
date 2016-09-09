7adf03df5d4453742d7b392704bf12fbad1e627b
Choi-PC\Choi <csj21300@naver.com>
2016-09-10 03:37:07 +0900

    v1.1 코딩한거 다 지워짐..

--- src/main/java/com/hanium/myapp/GPS/Terminal_Location/Terminal_Location.java
+++ src/main/java/com/hanium/myapp/GPS/Terminal_Location/Terminal_Location.java
@@ -67,9 +67,44 @@ public class Terminal_Location {
 	        System.out.println(items.toString());
 	        
 	        
+	        switch(answer)
+	        {
+	        case "1" :
+	        	
+	        	break;
+	        	
+	        case "2" :
+	        	
+	        	break;
+	        	
+	        case "3" :
+	        	
+	        	break;
+	        	
+	        case "4" :
+	        	
+	        	break;
+	        	
+	        case "5" :
+	        	
+	        	break;
+	        	
+	        case "6" :
+	        	
+	        	break;
+	        	
+	        case "7" :
+	        	
+	        	break;
+	        
+	        
+	        }
+	        
 	        
 	        
 	        return "0";
 	}
+	
+	
 	
 }
